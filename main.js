e2e56f9c3e04318a67d40d5e7717df357e588a37
phuef <phil.hueffer@gmail.com>
2020-05-01 17:52:20 +0200

    api

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ var APIKey= "9e55039796c9a5f733b4100a7273622d";
 // api.openweathermap.org/data/2.5/weather?lat=51.9541&lon=7.6210&appid=9e55039796c9a5f733b4100a7273622d
 var request = new XMLHttpRequest();
 var apicall="api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-request.open('GET', 'apicall', true);
+request.open('GET', apicall, true);
 request.onload = function() {
   // begin accessing JSON data here
   var data = JSON.parse(this.response);
