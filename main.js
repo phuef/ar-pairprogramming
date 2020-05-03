c8780b0de7a09774829a574eee696a0fdfe77e95
phuef <phil.hueffer@gmail.com>
2020-05-03 14:44:01 +0200

    api

--- main.js
+++ main.js
@@ -10,9 +10,9 @@ var APIKey= "9e55039796c9a5f733b4100a7273622d";
 // https://api.openweathermap.org/data/2.5/weather?lat=51.9541&lon=7.6210&appid=9e55039796c9a5f733b4100a7273622d
 var request = new XMLHttpRequest();
 var apicall="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid="+APIKey;
-async function weatherData(){
+function weatherData(){
   request.open('GET', apicall, true);
-   request.onload = await function() {
+   request.onload = function() {
     var data = JSON.parse(this.response);
   }
   //{"coord":{"lon":7.62,"lat":51.95},"weather":[{"id":521,"main":"Rain","description":"shower rain","icon":"09d"}],"base":"stations","main":{"temp":285.15,"feels_like":280.09,"temp_min":283.15,"temp_max":287.04,"pressure":1000,"humidity":71},"visibility":10000,"wind":{"speed":6.2,"deg":300},"clouds":{"all":40},"dt":1588347256,"sys":{"type":1,"id":1269,"country":"DE","sunrise":1588305552,"sunset":1588359227},"timezone":7200,"id":2867543,"name":"Münster","cod":200}
